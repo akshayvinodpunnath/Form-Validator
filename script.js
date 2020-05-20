926909ae87e4820963c0cfb4c5fede31bafc7bee
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-20 23:48:18 +0530

    checkEmail modified; checkPasswordsMatch function created for password validation

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
-function isEmailValid(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value)) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
 
 function checkRequiredField(inputArray) {
@@ -33,11 +37,29 @@ function checkRequiredField(inputArray) {
     })
 }
 
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    }
+}
+
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkPasswordsMatch(inputPassword, confirmPassword) {
+    if (inputPassword.value !== confirmPassword.value) {
+        showError(confirmPassword, `Passwords do not match`)
+    }
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     checkRequiredField([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 15);
+    checkEmail(email)
+    checkPasswordsMatch(password, password2)
 })
